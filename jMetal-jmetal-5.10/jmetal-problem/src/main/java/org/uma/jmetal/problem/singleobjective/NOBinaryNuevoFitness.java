a6fcece85545e995335e1978714127a404f0596d
Federico Gil <federico.gil@fing.edu.uy>
2022-11-07 18:52:58 -0300

    un avance de lo greedy

--- jMetal-jmetal-5.10/jmetal-problem/src/main/java/org/uma/jmetal/problem/singleobjective/NOBinaryNuevoFitness.java
+++ jMetal-jmetal-5.10/jmetal-problem/src/main/java/org/uma/jmetal/problem/singleobjective/NOBinaryNuevoFitness.java
@@ -11,7 +11,10 @@ import org.uma.jmetal.problem.singleobjective.grafo.Grafo;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.BitSet;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -23,7 +26,7 @@ public class NOBinaryNuevoFitness extends AbstractBinaryProblem {
 	private int numberOfNodes;
 	private Set<Edge> aristasOriginales;
 	private int bits ;
-	private static final long PRESUPUESTO = 500000L;
+	private static final Integer PRESUPUESTO = 500000;
 	private int cantidadAristasOriginales;
 	private int currentEvaluation;
 	private List<List<Integer>> posibleEdges;
@@ -31,10 +34,25 @@ public class NOBinaryNuevoFitness extends AbstractBinaryProblem {
 	private static final float ALPHA = 0.3f/12420000.0f;
 	private static final float BETA = 0.7f;
 
+	private static final Integer POPULATION_SIZE = 50;
+	private static final Integer MAX_EVALUATIONS = 15000;
+
+
+	//keep track of the fitness of the best solution in each generation
+	private Map<Integer,Double> bestFitnessPerGeneration;
+	//keep an array of the fitness of all the solutions in the current generation
+	private double[] currentFitness;
+
   /** Constructor */
   public NOBinaryNuevoFitness(Integer numberOfNodes, Set<Edge> oEdges) {
 	currentEvaluation = 0;
 	this.numberOfNodes = numberOfNodes;
+	
+	//keep track of the fitness of the best solution in each generation
+	bestFitnessPerGeneration = new HashMap<Integer,Double>();
+
+	//keep an array of the fitness of all the solutions in the current generation
+	currentFitness = new double[POPULATION_SIZE];
 
 	//store a copy of the original edges and not the reference
 	aristasOriginales = oEdges;
@@ -61,6 +79,7 @@ public class NOBinaryNuevoFitness extends AbstractBinaryProblem {
 			}
 		}
 	}
+
   }
 
 
@@ -83,8 +102,105 @@ public class NOBinaryNuevoFitness extends AbstractBinaryProblem {
 
   @Override
   public BinarySolution createSolution() {
-    return new DefaultBinarySolution(getListOfBitsPerVariable(), getNumberOfObjectives()) ;
+	//with a 50% chance, the solution will be greedy or random
+	if (Math.random() < 0.1) {
+		System.out.println("greedy solution"); 
+		return greedy(PRESUPUESTO);
+	} else {
+		System.out.println("random solution");
+		return new DefaultBinarySolution(getListOfBitsPerVariable(), getNumberOfObjectives()) ;
+	}
   }
+
+  public BinarySolution greedy(int budget) {
+	//posible edges
+	Set<Edge> edges = aristasOriginales;
+	Integer nVertices = numberOfNodes;
+	List<List<Integer>> posibleEdges;
+	posibleEdges = new ArrayList<List<Integer>>();
+	for (int i = 1; i <= nVertices; i++) {
+		for (int j = i+1; j <= nVertices; j++) {
+			List<Integer> edge = new ArrayList<Integer>();
+			edge.add(i);
+			edge.add(j);
+			// if the edge is not in the original graph, add it to the list of posible edges, without using contains
+			if (!edges.contains(new Edge(i,j,0f,0f,true))) {
+				posibleEdges.add(edge);
+			}
+		}
+	}
+
+	//create the original graph
+	Grafo g = new Grafo(nVertices, edges);
+
+	//update a sample of the graph
+	g = g.updateSample();
+
+	//create a map that will store the degree of each vertex
+	Map<Integer, Integer> degree = new HashMap<Integer, Integer>();
+	for (int i = 1; i <= nVertices; i++) {
+		degree.put(i,0);
+	}
+
+	//calculate the degree of each vertex in the sampled graph
+	for (Edge e : g.getEdges()) {
+		degree.put(e.getV1(), degree.get(e.getV1())+1);
+		degree.put(e.getV2(), degree.get(e.getV2())+1);
+	}
+
+	//while in budget add an edge between the two (diferent) vertices with the lowers degree
+	while (budget - COST_PER_KILOMETER > 0) {
+		//find the two vertices with the lowest degree
+		int min1 = Integer.MAX_VALUE;
+		int min2 = Integer.MAX_VALUE;
+		int v1 = 0;
+		int v2 = 0;
+		for (int i = 1; i <= nVertices; i++) {
+			if (degree.get(i) < min1) {
+				min2 = min1;
+				v2 = v1;
+				min1 = degree.get(i);
+				v1 = i;
+			} else if (degree.get(i) < min2) {
+				min2 = degree.get(i);
+				v2 = i;
+			}
+		}
+
+		//add the edge to the graph
+		g.addEdge(new Edge(v1,v2,0.05f,1.0f,true));
+
+		//update the degree of the vertices
+		degree.put(v1, degree.get(v1)+1);
+		degree.put(v2, degree.get(v2)+1);
+
+		//update the budget
+		budget -= COST_PER_KILOMETER;
+	}
+
+	//create the solution
+	BinarySolution individual = new DefaultBinarySolution(getListOfBitsPerVariable(), getNumberOfObjectives());
+
+	//get the edges of the resulting graph
+	Set<Edge> edgesResult = g.getEdges();
+
+
+	//first set all the bits to 0
+	for (int i = 0; i < bits; i++) {
+		individual.getVariable(0).set(i, false);
+	}
+
+	//for each new edge, set the corresponding bit to 1 in the solution where the bits are ordered by the posible edges
+	int i = 0;
+	for (List<Integer> edge : posibleEdges) {
+		if (edgesResult.contains(new Edge(edge.get(0),edge.get(1),0f,0f,true)) || edgesResult.contains(new Edge(edge.get(1),edge.get(0),0f,0f,true))) {
+			individual.getVariable(0).set(i, true);
+		}
+		i++;
+	}
+	return individual;
+}
+
 
 
   /** Evaluate() method */
@@ -134,13 +250,47 @@ public class NOBinaryNuevoFitness extends AbstractBinaryProblem {
 		  fitness *= 1e6;
 	}
 
+	//store the fitness to currentFitness in the position of the current evaluation modulo the population size
+	currentFitness[currentEvaluation % POPULATION_SIZE] = fitness;
+
+
 	  //print the current fitness every 100 evaluations, asuming there are 25000 evaluations print the progress porcentage as well
-	  if (currentEvaluation % 100 == 0) {
-		  System.out.println("Current fitness: " + fitness + " Progress: " +((currentEvaluation/15000f)*100) + " %" + " Costo Update: " + costoUpdate);
+	 // if (currentEvaluation % 100 == 0) {
+	//	  System.out.println("Current fitness: " + fitness + " Progress: " +((currentEvaluation/15000f)*100) + " %" + " Costo Update: " + costoUpdate);
+	  //}
+
+	  if (currentEvaluation % POPULATION_SIZE == 0) {
+		  //calculate the average fitness of the population
+		  /*
+		  double averageFitness = 0;
+		  for (int i = 0; i < POPULATION_SIZE; i++) {
+			  averageFitness += currentFitness[i];
+		  }
+		  averageFitness /= POPULATION_SIZE;
+		  */
+		  //calculate the best fitness of the population
+		  double bestFitness = Double.MAX_VALUE;
+		  for (int i = 0; i < POPULATION_SIZE; i++) {
+			  if (currentFitness[i] < bestFitness) {
+				  bestFitness = currentFitness[i];
+			  }
+		  }
+
+
+		  //print the average fitness of the population
+		  System.out.println("Average fitness: " + bestFitness);
+
+		  //add the value to the map
+		  bestFitnessPerGeneration.put(currentEvaluation/POPULATION_SIZE, bestFitness);
 	  }
 
 
 	  solution.setObjective(0, fitness);
+  }
+
+  //a getter for the map bestFitnessPerGeneration
+  public Map<Integer, Double> getBestFitnessPerGeneration() {
+	  return bestFitnessPerGeneration;
   }
   
   public int[] proy(int i) {
